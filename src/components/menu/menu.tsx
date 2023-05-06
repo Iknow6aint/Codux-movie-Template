04a8481ff0f14a1d0b54806c7b2a2f5f830b0b05
iknow6aint <saintagbukor@gmail.com>
2023-05-06 22:21:32 +0100

    menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -12,10 +12,15 @@ export interface MenuProps {
 export const Menu = ({ className }: MenuProps) => {
     return (
         <div className={classNames(styles.root, className)}>
-            <div>
-                <img />
-                <span>iknow movies</span>
+            <div className={styles.logo}>
+                <img
+                    src="https://app.logo.com/view/logo_20d72b1a-686f-41aa-9ebf-1a278346fcce"
+                    className={styles.logoImg}
+                />
+                <span className={styles.logoText}>iknow movies</span>
             </div>
+            <span className={styles.title}>Sort By</span>
+            <hr className={styles.hr} />
         </div>
     );
 };
